2336ea6018d6150f214cb9004068d074a7d64f18
Kokila11 <kokila1105@gmail.com>
2016-07-28 17:23:43 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class odd
+{
+	public static void main(String[] arg)
+	{
+
+	Scanner in=new Scanner(System.in)
+	int n;
+	n=in.nextInt();
+	if(n%2==0)
+		System.out.println(n + "is even ");
+	else
+		System.out.println(n + "is odd ");
+
+	}
+
+}
