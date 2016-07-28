38c07859682992e9d69fde0035ed1de9b437dd7f
Kokila11 <kokila1105@gmail.com>
2016-07-28 17:29:46 +0530

    Add files via upload

---
+++ pos.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class pos
+{
+	public static void main(String[] arg)
+	{
+
+	Scanner in=new Scanner(System.in)
+	int n;
+	n=in.nextInt();
+	if(n>0)
+		System.out.println(n + "is positive ");
+	else
+		System.out.println(n + "is negative ");
+
+	}
+
+}
