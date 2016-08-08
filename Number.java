ec2dacad4df1f57684ec17cff0cac6577cc5b7a4
Kokila11 <kokila1105@gmail.com>
2016-08-08 06:04:25 -0700

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Number
+{
+public static void main(String[],arg)
+{
+int a,i;
+Scanner in=new Scanner(System.in);
+a=in.nextInt();
+if(i>10&&i<50)
+{
+	if(a%2==0)
+
+System.out.println(a+"is even number");
+else
+
+System.out.println(a+"is not even number");
+}
+}
+}
