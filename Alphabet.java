f6f0998868f2d76108c58bb73b5f7b8cf4853b5d
Kokila11 <kokila1105@gmail.com>
2016-08-08 05:27:31 -0700

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Alphabet
+{
+public static void main(String[],arg)
+{
+char i;
+if((i>='a'&&i<='z')||(i>='A'&&i<='Z'))
+
+System.out.println(i+"is alphabet");
+else
+System.out.println(i+"not an alphabet");
+}
+}
