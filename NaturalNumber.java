66b348766c030545ce657dfdab47fcda05f02cca
Kokila11 <kokila1105@gmail.com>
2016-08-09 17:44:46 +0530

    Add files via upload

---
+++ NaturalNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class NaturalNumber
+{
+public static void main(String[] arg)
+{
+int n,i,a=0;
+n=in.nextInt();
+for(i=1;i<=n;i++)
+{
+a=a+i;
+}
+System.out.println(a);
+}
+}
